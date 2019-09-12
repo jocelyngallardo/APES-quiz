8c7bf7248c62c225de4f2a7c551d0a013c76d0bd
kedehlsen <kedehlsen@gmail.com>
2019-09-12 09:42:40 -0700

    add questions 1-3

--- APES-jQuery.js
+++ APES-jQuery.js
@@ -1,8 +1,56 @@
 $(document).ready(function(){
 	
-	$("#button1").click(function(){
-		$("#button1").hide();
+	$("#showanswer").hide();
+	$("#question1").hide();
+	$("#question2").hide();
+	$("#question3").hide();
+	$("#next").hide();
+	$("#answer1").hide();
+	$("#answer2").hide();
+	$("#answer3").hide();
+	
+	$("#start").click(function(){
+		$("#start").hide();
 		$("#question1").show();
+		$("#showanswer").show();
+	});
+	
+	$("#showanswer").click(function(){
+		$("#showanswer").hide();
+		$("#answer1").show();
+		$("#next").show();
+	});
+	
+	$("#next").click(function(){
+		$("#next").hide();
+		$("#question1").hide();
+		$("#answer1").hide();
+		$("#question2").show();
+		$("#answer2").hide();
+		$("#showanswer").show();
+	});
+	
+	$("#showanswer").click(function(){
+		$("#showanswer").hide();
+		$("#answer1").hide();
+		$("#answer2").show();
+		$("#next").show();
+	});
+	
+	$("#next").click(function(){
+		$("#next").hide();
+		$("#question2").hide();
+		$("#answer2").hide();
+		$("#question3").show();
+		$("#answer3").hide();
+		$("#showanswer").show();
+	});
+	
+	$("#showanswer").click(function(){
+		$("#showanswer").hide();
+		$("#answer2").hide();
+		$("#answer3").show();
+		$("#next").show();
 	});
 	
 });
