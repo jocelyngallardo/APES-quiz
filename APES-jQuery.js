5de543c9b3bedb3ff6e72641486b3362a947f03e
kedehlsen <kedehlsen@gmail.com>
2019-09-13 09:39:44 -0700

    made it work

--- APES-jQuery.js
+++ APES-jQuery.js
@@ -1,56 +1,29 @@
 $(document).ready(function(){
 	
-	$("#showanswer").hide();
-	$("#question1").hide();
-	$("#question2").hide();
-	$("#question3").hide();
-	$("#next").hide();
-	$("#answer1").hide();
-	$("#answer2").hide();
-	$("#answer3").hide();
+	var q="question";
+	var count=1;
 	
 	$("#start").click(function(){
 		$("#start").hide();
-		$("#question1").show();
+		$("#question"+count).show();
 		$("#showanswer").show();
+		$("#showanswer").click(function(){
+			$("#showanswer").hide();
+			$("#answer"+count).show();
+			$("#next").show();
+		});
+		$("#next").click(function(){
+			$("#next").hide();
+			$("#question"+count).hide();
+			$("#answer"+count).hide();
+			count++;
+			$("#question"+count).show();
+			$("#showanswer").show();
+
+		});
+		
 	});
 	
-	$("#showanswer").click(function(){
-		$("#showanswer").hide();
-		$("#answer1").show();
-		$("#next").show();
-	});
-	
-	$("#next").click(function(){
-		$("#next").hide();
-		$("#question1").hide();
-		$("#answer1").hide();
-		$("#question2").show();
-		$("#answer2").hide();
-		$("#showanswer").show();
-	});
-	
-	$("#showanswer").click(function(){
-		$("#showanswer").hide();
-		$("#answer1").hide();
-		$("#answer2").show();
-		$("#next").show();
-	});
-	
-	$("#next").click(function(){
-		$("#next").hide();
-		$("#question2").hide();
-		$("#answer2").hide();
-		$("#question3").show();
-		$("#answer3").hide();
-		$("#showanswer").show();
-	});
-	
-	$("#showanswer").click(function(){
-		$("#showanswer").hide();
-		$("#answer2").hide();
-		$("#answer3").show();
-		$("#next").show();
-	});
-	
+
+
 });
